5dc2942741857e5ca5d43910805be2611a4f4f3c
علوش <anldhe.b42.9@gmail.com>
2025-01-18 18:51:55 +0300

    Add files via upload

---
+++ FallenMusic/Helpers/admins.py
@@ -0,0 +1,69 @@
+from typing import Callable
+
+from pyrogram.enums import ChatMemberStatus
+from pyrogram.types import CallbackQuery, Message
+
+from FallenMusic import SUDOERS, app
+
+from .active import is_active_chat
+
+
+def admin_check(func: Callable) -> Callable:
+    async def non_admin(_, message: Message):
+        if not await is_active_chat(message.chat.id):
+            return await message.reply_text("لا يتم تشغيل البوت عبر دردشة الفيديو.")
+
+        if message.from_user.id in SUDOERS:
+            return await func(_, message)
+
+        check = await app.get_chat_member(message.chat.id, message.from_user.id)
+        if check.status not in [ChatMemberStatus.OWNER, ChatMemberStatus.ADMINISTRATOR]:
+            return await message.reply_text(
+                "✎┊‌ ʏᴏᴜ'ʀᴇ ɴᴏᴛ ᴀɴ ᴀᴅᴍɪɴ ʙᴀʙʏ, ᴘʟᴇᴀsᴇ sᴛᴀʏ ɪɴ ʏᴏᴜʀ ʟɪᴍɪᴛs."
+            )
+
+        admin = (
+            await app.get_chat_member(message.chat.id, message.from_user.id)
+        ).privileges
+        if admin.can_manage_video_chats:
+            return await func(_, message)
+        else:
+            return await message.reply_text(
+                "✎┊‌ ليس لديك أذونات لإدارة محادثات الفيديو "
+            )
+
+    return non_admin
+
+
+def admin_check_cb(func: Callable) -> Callable:
+    async def cb_non_admin(_, query: CallbackQuery):
+        if not await is_active_chat(query.message.chat.id):
+            return await query.answer(
+                "ʙᴏᴛ ɪsɴ'ᴛ sᴛʀᴇᴀᴍɪɴɢ ᴏɴ ᴠɪᴅᴇᴏᴄʜᴀᴛ.", show_alert=True
+            )
+
+        if query.from_user.id in SUDOERS:
+            return await func(_, query)
+
+        try:
+            check = await app.get_chat_member(query.message.chat.id, query.from_user.id)
+        except:
+            return
+        if check.status not in [ChatMemberStatus.OWNER, ChatMemberStatus.ADMINISTRATOR]:
+            return await query.answer(
+                "✎┊‌ انت لست مشرف 🥺",
+                show_alert=True,
+            )
+
+        admin = (
+            await app.get_chat_member(query.message.chat.id, query.from_user.id)
+        ).privileges
+        if admin.can_manage_video_chats:
+            return await func(_, query)
+        else:
+            return await query.answer(
+                "✎┊‌ ليس لديك أذونات لإدارة محادثات الفيديو",
+                show_alert=True,
+            )
+
+    return cb_non_admin
---
+++ FallenMusic/Helpers/inline.py
@@ -0,0 +1,88 @@
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+
+import config
+from FallenMusic import BOT_USERNAME
+
+close_key = InlineKeyboardMarkup(
+    [[InlineKeyboardButton(text="• مسح •", callback_data="close")]]
+)
+
+
+buttons = InlineKeyboardMarkup(
+    [
+        [
+            InlineKeyboardButton(text="▷", callback_data="resume_cb"),
+            InlineKeyboardButton(text="II", callback_data="pause_cb"),
+            InlineKeyboardButton(text="‣‣I", callback_data="skip_cb"),
+            InlineKeyboardButton(text="▢", callback_data="end_cb"),
+        ]
+    ]
+)
+
+
+pm_buttons = [
+    [
+        InlineKeyboardButton(
+            text="‹ اضف البوت في مجموعتك ›",
+            url=f"https://t.me/{BOT_USERNAME}?startgroup=true",
+        )
+    ],
+    [InlineKeyboardButton(text="• اوامࢪ التشغيل •", callback_data="fallen_help")],
+    [
+        InlineKeyboardButton(text="• سوࢪس القرش •", url=config.SUPPORT_CHANNEL),
+        InlineKeyboardButton(text="• جࢪوب الدعم •", url=config.SUPPORT_CHAT),
+    ],
+    [
+        InlineKeyboardButton(
+            text="• مطوࢪ السورس •", url="https://t.me/I_e_e_l"
+        ),
+        InlineKeyboardButton(text="• مالك البوت •", user_id=config.OWNER_ID),
+    ],
+]
+
+
+gp_buttons = [
+    [
+        InlineKeyboardButton(
+            text="‹ اضف البوت في مجموعتك ›",
+            url=f"https://t.me/{BOT_USERNAME}?startgroup=true",
+        )
+    ],
+    [
+        InlineKeyboardButton(text="• سوࢪس القرش •", url=config.SUPPORT_CHANNEL),
+        InlineKeyboardButton(text="• جࢪوب الدعم •", url=config.SUPPORT_CHAT),
+    ],
+    [
+        InlineKeyboardButton(
+            text="• مطوࢪ السورس •", url="https://t.me/I_e_e_l"
+        ),
+        InlineKeyboardButton(text="• مالك البوت •", user_id=config.OWNER_ID),
+    ],
+]
+
+
+helpmenu = [
+    [
+        InlineKeyboardButton(
+            text="• اوامࢪ التشغيل •",
+            callback_data="fallen_cb help",
+        )
+    ],
+    [
+        InlineKeyboardButton(text="• اوامࢪ المطور •", callback_data="fallen_cb sudo"),
+        InlineKeyboardButton(text="• اوامࢪ المالك •", callback_data="fallen_cb owner"),
+    ],
+    [
+        InlineKeyboardButton(text="• ࢪجوع •", callback_data="fallen_home"),
+        InlineKeyboardButton(text="• مسح •", callback_data="close"),
+    ],
+]
+
+
+help_back = [
+    [InlineKeyboardButton(text="• جࢪوب الدعم •", url=config.SUPPORT_CHAT)],
+    [
+        InlineKeyboardButton(text="• ࢪجوع •", callback_data="fallen_help"),
+        InlineKeyboardButton(text="• مسح •", callback_data="close"),
+    ],
+]
